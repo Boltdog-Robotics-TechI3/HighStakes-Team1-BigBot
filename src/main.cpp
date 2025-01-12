9cbbe65b568c5e2caca9675194ec88eca991140f
Aiden-Koon <aiaub1212@gmail.com>
2025-01-11 18:56:43 -0600

    Updated OPcontrol and started work on autonomous
    Co-authored-by: Rachel Dahl <RaeDahl@users.noreply.github.com>
    Co-authored-by: Reid-Jourdan <Reid-Jourdan@users.noreply.github.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,49 @@
 #include "main.h"
 
 pros::Controller master(pros::E_CONTROLLER_MASTER);
-pros::Motor lift(10);
+pros::Motor lift(16);
 
 std::shared_ptr<ChassisController> chassis = ChassisControllerBuilder()
-	.withMotors({-11, 12, -13}, {1, -2, 3})
-	.withDimensions(AbstractMotor::gearset::blue, {{3.25_in, 11.5_in}, imev5BlueTPR})
+	.withMotors({-18, 19, -20}, {11, -12, 13})
+	.withDimensions({AbstractMotor::gearset::blue, (84.0/60.0)}, {{4_in, 15.25_in}, imev5BlueTPR})
 	.build();
 
 std::shared_ptr<ChassisModel> drivetrain = chassis->getModel();
 
-pros::adi::Pneumatics clamp = pros::adi::Pneumatics('H', false);
+pros::adi::Pneumatics mogoClamp = pros::adi::Pneumatics('H', false);
+
+std::shared_ptr<AsyncMotionProfileController> profileController = 
+  AsyncMotionProfileControllerBuilder()
+    .withLimits({
+      1.0, // Maximum linear velocity of the Chassis in m/s
+      2.0, // Maximum linear acceleration of the Chassis in m/s/s
+      10.0 // Maximum linear jerk of the Chassis in m/s/s/s
+    })
+    .withOutput(chassis)
+    .buildMotionProfileController();
 
 
+/* move 4 in
+	get ring
+	back 4 in
+	score ring
+   turn 45
+   move forward 2 ft
+*/
+void skills_autonomous() {
+	chassis->setMaxVelocity(300);
+	lift.move(127);
+	chassis -> moveDistance(4_in);
+	pros::delay(500);
+	chassis -> moveDistance(-2.25_in);
+	pros::delay(500);
+	chassis -> moveDistance(16_in);
+	chassis -> turnAngle(45_deg);
+	chassis -> moveDistance(28_in);
+	pros::delay(2000);
+	lift.move(0);
+	chassis -> turnAngle(-135_deg);
+}
 /**
  * A callback function for LLEMU's center button.
  *
@@ -88,14 +119,30 @@ void autonomous() {}
  * task, not resume it from where it left off.
  */
 void opcontrol() {
+	//Controls:
+	//B button toggles clamp
+	//A button controls intake and lift
+	//Left joystick controls forward/backwards movement
+	//Right joystick controls turning
+
 	while (true) {
-		int dir = master.get_analog(ANALOG_LEFT_Y);    // Gets amount forward/backward from left joystick
+		int dir = (pow((master.get_analog(ANALOG_LEFT_Y) / 127.0), 3) * 127.0);    // Gets amount forward/backward from left joystick
 		int turn = master.get_analog(ANALOG_RIGHT_X);  // Gets the turn left/right from right joystick
 
-		if (master.get_digital(DIGITAL_A)) {
+		if (master.get_digital(DIGITAL_R1)) { 
 			lift.move(127);
+		} else if (master.get_digital(DIGITAL_R2)) {
+			lift.move(-127);
 		} else {
 			lift.move(0);
+		}
+
+		if (master.get_digital_new_press(DIGITAL_B)) { 
+			mogoClamp.toggle();
+		}
+
+		if (master.get_digital(DIGITAL_RIGHT)) {
+			skills_autonomous();
 		}
 		
 		drivetrain->arcade(dir, turn);
